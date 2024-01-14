1c7dc3ed3887177eae82495718d28ebf2e8060cc
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2024-01-14 16:05:57 -0300

    refactor(api): remove '?' from the endpoints and add them on the 'api_call' instead

--- lib/data/api/api.dart
+++ lib/data/api/api.dart
@@ -5,8 +5,8 @@ class Api {
   static const String baseUrl = 'https://api.themoviedb.org/3/';
 
   // Endpoints
-  static const String seriesTopRatedEndpoint = 'tv/top_rated?';
-  static const String seriesPopularityEndpoint = 'tv/popular?';
-  static const String movieTopRatedEndpoint = 'movie/top_rated?';
-  static const String moviePopularityEndpoint = 'movie/popular?';
+  static const String seriesTopRatedEndpoint = 'tv/top_rated';
+  static const String seriesPopularityEndpoint = 'tv/popular';
+  static const String movieTopRatedEndpoint = 'movie/top_rated';
+  static const String moviePopularityEndpoint = 'movie/popular';
 }

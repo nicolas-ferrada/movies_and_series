b2762b039428dc8dc92f2ccba637f139ca1bb7de
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2024-01-14 14:00:51 -0300

    feat: api class to store api data

---
+++ lib/data/api/api.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Api {
+  static String? key = dotenv.env['APIKEY'];
+  static const String baseUrl = 'https://api.themoviedb.org/3/';
+
+  // Endpoints
+  static const String seriesTopRatedEndpoint = 'tv/top_rated?';
+  static const String seriesPopularityEndpoint = 'tv/popular?';
+  static const String movieTopRatedEndpoint = 'movie/top_rated?';
+  static const String moviePopularityEndpoint = 'movie/popular?';
+}

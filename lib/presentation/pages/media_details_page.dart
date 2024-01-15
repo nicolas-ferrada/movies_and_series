fb1d33e7442a32b38777a917f7d223a5a5e324fa
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2024-01-15 01:53:47 -0300

    feat: add 'media details page' to display info about the media

--- lib/presentation/pages/media_details_page.dart
+++ lib/presentation/pages/media_details_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../data/api/api.dart';
 import '../../data/models/media.dart';
 
 class MediaDetailsPage extends StatelessWidget {
@@ -12,8 +13,47 @@ class MediaDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          media.name,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 14),
+        ),
+      ),
       body: SafeArea(
-        child: Text(media.name),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.network(Api.imageBaseUrl + media.backdropPath),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    media.name,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text('Release date: ${media.releaseDate}'),
+                Text('Rating: ${media.rating} (${media.rateCount} votes)'),
+                Text('Popularity: ${media.popularity}'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    media.overview,
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }

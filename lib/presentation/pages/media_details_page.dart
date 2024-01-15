993cc3c4922faad76125f3e22ec771b75158cc97
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2024-01-15 14:10:43 -0300

    refactor(media details page): improve code structure and add padding to each item

--- lib/presentation/pages/media_details_page.dart
+++ lib/presentation/pages/media_details_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../data/models/media.dart';
+import '../widgets/component_widget/app_bar_media_details_component.dart';
 import '../widgets/reusable_widgets/media_image.dart';
 
 class MediaDetailsPage extends StatelessWidget {
@@ -13,61 +14,91 @@ class MediaDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          media.name,
-          textAlign: TextAlign.center,
-          style: const TextStyle(fontSize: 14),
-        ),
-      ),
+      appBar: AppBarMediaDetailsComponent(mediaName: media.name),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Hero(
-                  tag: media.id,
-                  child: MediaImage(
-                    mediaUrl: media.posterPath,
-                    mediaId: media.id,
-                    height: 300,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    media.name,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text('Release date: ${media.releaseDate}'),
-                Text('Rating: ${media.rating} (${media.rateCount} votes)'),
-                Text('Popularity: ${media.popularity}'),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    media.overview,
-                    textAlign: TextAlign.justify,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: MediaImage(
-                    mediaUrl: media.backdropPath,
-                    mediaId: media.id,
-                    width: 300,
-                  ),
-                ),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  mediaImageCover(),
+                  releaseDate(),
+                  rating(),
+                  popularity(),
+                  overview(),
+                  mediaImageBackdrop(),
+                ],
+              ),
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget mediaImageBackdrop() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: MediaImage(
+        mediaUrl: media.backdropPath,
+        mediaId: media.id,
+        width: 300,
+      ),
+    );
+  }
+
+  Widget overview() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        media.overview,
+        textAlign: TextAlign.justify,
+      ),
+    );
+  }
+
+  Widget popularity() {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Text(
+        'Popularity: ${media.popularity}',
+        style: const TextStyle(fontSize: 16),
+      ),
+    );
+  }
+
+  Widget rating() {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Text(
+        'Rating: ${media.rating} (${media.rateCount} votes)',
+        style: const TextStyle(fontSize: 16),
+      ),
+    );
+  }
+
+  Widget releaseDate() {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Text(
+        'Release date: ${media.releaseDate}',
+        style: const TextStyle(fontSize: 16),
+      ),
+    );
+  }
+
+  Widget mediaImageCover() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 0, bottom: 8),
+      child: Hero(
+        tag: media.id,
+        child: MediaImage(
+          mediaUrl: media.posterPath,
+          mediaId: media.id,
+          height: 300,
         ),
       ),
     );
